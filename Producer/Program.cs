d72001ecc4f16307eb473bfb27768264f455a87c
Willem Botha <willemb@agilebridge.co.za>
2022-04-08 11:45:09 +0200

    Changed to parra foreach

--- Producer/Program.cs
+++ Producer/Program.cs
@@ -21,33 +21,33 @@ List<string> authorIds = new List<string>()
     "raj-kumar-beniwal"
 };
 
-foreach (var author in authorIds)
+Parallel.ForEach(authorIds, new ParallelOptions { MaxDegreeOfParallelism = 100 }, author =>
 {
-    
+
     XDocument doc = XDocument.Load("https://www.c-sharpcorner.com/members/" + author + "/rss");
     if (doc == null)
     {
         Console.WriteLine("Bad Bad, very bad");
     }
     var entries = from item in doc.Root.Descendants().First(i => i.Name.LocalName == "channel").Elements().Where(i => i.Name.LocalName == "item")
-        select new Feed
-        {
-            Content = item.Elements().First(i => i.Name.LocalName == "description").Value,
-            Link = (item.Elements().First(i => i.Name.LocalName == "link").Value).StartsWith("/") ? "https://www.c-sharpcorner.com" + item.Elements().First(i => i.Name.LocalName == "link").Value : item.Elements().First(i => i.Name.LocalName == "link").Value,
-            PubDate = Convert.ToDateTime(item.Elements().First(i => i.Name.LocalName == "pubDate").Value, culture),
-            Title = item.Elements().First(i => i.Name.LocalName == "title").Value,
-            FeedType = (item.Elements().First(i => i.Name.LocalName == "link").Value).ToLowerInvariant().Contains("blog") ? "Blog" : (item.Elements().First(i => i.Name.LocalName == "link").Value).ToLowerInvariant().Contains("news") ? "News" : "Article",
-            Author = item.Elements().First(i => i.Name.LocalName == "author").Value
-        };
+                  select new Feed
+                  {
+                      Content = item.Elements().First(i => i.Name.LocalName == "description").Value,
+                      Link = (item.Elements().First(i => i.Name.LocalName == "link").Value).StartsWith("/") ? "https://www.c-sharpcorner.com" + item.Elements().First(i => i.Name.LocalName == "link").Value : item.Elements().First(i => i.Name.LocalName == "link").Value,
+                      PubDate = Convert.ToDateTime(item.Elements().First(i => i.Name.LocalName == "pubDate").Value, culture),
+                      Title = item.Elements().First(i => i.Name.LocalName == "title").Value,
+                      FeedType = (item.Elements().First(i => i.Name.LocalName == "link").Value).ToLowerInvariant().Contains("blog") ? "Blog" : (item.Elements().First(i => i.Name.LocalName == "link").Value).ToLowerInvariant().Contains("news") ? "News" : "Article",
+                      Author = item.Elements().First(i => i.Name.LocalName == "author").Value
+                  };
 
     List<Feed> feeds = entries.OrderByDescending(o => o.PubDate).ToList();
     Console.WriteLine("Feeds Found, {0} for Author {1}", feeds.Count, author);
     for (int i = 0; i < feeds.Count; i++)
     {
-       var report = producer.ProduceAsync(feeds[i]).GetAwaiter().GetResult();
-       Console.WriteLine("Produced: {0}", report.Value);
+        var report = producer.ProduceAsync(feeds[i]).GetAwaiter().GetResult();
+        Console.WriteLine("Produced: {0}", report.Value);
     }
-}
+});
 
 
 
