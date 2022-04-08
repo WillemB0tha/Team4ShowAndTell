4b294f20939aab33e1df98d02f3b13aa23220a31
JacquesBronk <jacquesb@agilebridge.co.za>
2022-04-08 10:49:02 +0200

    Consumer Builder

---
+++ Consumer/ConsumerWorker.cs
@@ -0,0 +1,53 @@
+﻿using System.ComponentModel;
+using Confluent.Kafka;
+
+namespace Consumer;
+
+public class ConsumerWorker<T>
+{
+    private readonly string? _host;
+    private readonly int _port;
+    private readonly string _topic;
+
+    public ConsumerWorker()
+    {
+        _host = "localhost";
+        _port = 9092;
+        _topic = "ProducerLog";
+    }
+
+    ConsumerConfig GetConsumerConfig()
+    {
+        return new ConsumerConfig()
+        {
+            BootstrapServers = $"{_host}:{_port}",
+            GroupId = "CornerData",
+            AutoOffsetReset = AutoOffsetReset.Earliest;
+        };
+    }
+
+    public async Task Consume()
+    {
+        using (var consumer = new ConsumerBuilder<Ignore, T>(GetConsumerConfig())
+                   .SetValueDeserializer(new ValueDeserializer<T>()).Build())
+        {
+            consumer.Subscribe(_topic);
+
+            await Task.Run(() =>
+            {
+                while (true)
+                {
+                    var consumerResult = consumer.Consume(default(CancellationToken));
+                    if (consumerResult.Message.Value is Feed)
+                    {
+                        //Save Here
+                    }
+                }
+            });
+            
+            consumer.Close();
+        }
+    }
+    
+    
+}
---
+++ Consumer/CornerDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Consumer;
+
+public class CornerDbContext: DbContext
+{
+    public CornerDbContext(DbContextOptions<CornerDbContext> options): base(options)
+    {
+        
+    }
+    public DbSet<ArticleMatrix>? ArticleMatrices { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Server=localhost,1433;Database=CScrape;User Id=sa;Password=Password123;");
+    }
+    
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+    }
+    
+    
+}
