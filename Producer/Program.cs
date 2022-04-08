b1918d61cdf5936e4d67ab3cc55051512909f0a7
Jurie <jor@agilebridge.co.za>
2022-04-08 13:16:48 +0200

    added cache

--- Producer/Program.cs
+++ Producer/Program.cs
@@ -107,12 +107,17 @@ List<string> authorIds = new List<string>()
 
 Parallel.ForEach(authorIds, new ParallelOptions { MaxDegreeOfParallelism = 1000 }, author =>
 {
-    XDocument doc = XDocument.Load("https://www.c-sharpcorner.com/members/" + author + "/rss");
-    if (doc == null)
+    XDocument authorDoc;
+    if (!_memoryCache.TryGetValue(author, out XDocument cacheValue))
     {
-        Console.WriteLine("Bad Bad, very bad");
+        cacheValue = XDocument.Load("https://www.c-sharpcorner.com/members/" + author + "/rss");
+        if (authorDoc == null)
+        {
+            Console.WriteLine("Bad Bad, very bad");
+        }
     }
-    var entries = from item in doc.Root.Descendants().First(i => i.Name.LocalName == "channel").Elements().Where(i => i.Name.LocalName == "item")
+    authorDoc = cacheValue;
+    var entries = from item in authorDoc.Root.Descendants().First(i => i.Name.LocalName == "channel").Elements().Where(i => i.Name.LocalName == "item")
                   select new Feed
                   {
                       Content = item.Elements().First(i => i.Name.LocalName == "description").Value,
@@ -124,12 +129,27 @@ Parallel.ForEach(authorIds, new ParallelOptions { MaxDegreeOfParallelism = 1000 
                   };
 
     List<Feed> feeds = entries.OrderByDescending(o => o.PubDate).ToList();
-    Console.WriteLine("Feeds Found, {0} for Author {1}", feeds.Count, author);
+
+    Console.WriteLine("Feeds Found, {0} for Author {1}", feeds.Count, author); cacheValue = author;
+
+    var cacheEntryOptions = new MemoryCacheEntryOptions()
+        .SetSlidingExpiration(TimeSpan.FromSeconds(3));
+
+    XDocument doc = XDocument.Load("https://www.c-sharpcorner.com/members/" + author + "/rss");
+    if (doc == null)
+    {
+        Console.WriteLine("Bad Bad, very bad");
+    }
+
+    authorDoc = doc;
+
+    _memoryCache.Set(author, authorDoc, cacheEntryOptions);
 
     for (int i = 0; i < feeds.Count; i++)
     {
         try
         {
+
             var report = kafkaproducer.ProduceAsync(feeds[i]).GetAwaiter().GetResult();
             Console.WriteLine("Produced: {0}", report.Value);
 
