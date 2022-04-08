514c2b8eaf1f1c97a16e8c6474f723d7801ceae0
Willem Botha <willemb@agilebridge.co.za>
2022-04-08 09:34:37 +0200

    Added NBomber

---
+++ NBomberTest/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Net.Http;
+
+using NBomber;
+using NBomber.Contracts;
+using NBomber.CSharp;
+
+namespace NBomberTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            using var httpClient = new HttpClient();
+         
+
+            var step = Step.Create("fetch_html_page", async context =>
+            {
+                var response = await httpClient.GetAsync("https://nbomber.com");
+
+                return response.IsSuccessStatusCode
+                    ? Response.Ok()
+                    : Response.Fail();
+            });
+
+            var scenario = ScenarioBuilder.CreateScenario("simple_http", step).WithWarmUpDuration(TimeSpan.FromSeconds(5))
+                .WithLoadSimulations(
+                    Simulation.InjectPerSec(rate: 100, during: TimeSpan.FromSeconds(30))
+                );;
+
+            NBomberRunner
+                .RegisterScenarios(scenario)
+                .Run();
+        }
+    }
+}
